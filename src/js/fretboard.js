52c3846a0f65cac4abe54fb146cd15b5640f4256
Ramiro Gómez <code@ramiro.org>
2021-06-29 02:08:56 +0200

    Use const when possible and improve note label positioning

--- src/js/fretboard.js
+++ src/js/fretboard.js
@@ -3,7 +3,7 @@ import {noteAtPosition, noteIndex, noteLabel} from './notes';
 
 
 function drawCircle(parent, cx, cy, radius, fill='#cccccc', stroke=null, stroke_width=null, title=null) {
-    let circle = parent.append('svg:circle')
+    const circle = parent.append('svg:circle')
         .attr('cx', cx)
         .attr('cy', cy)
         .attr('r', radius)
@@ -16,52 +16,52 @@ function drawCircle(parent, cx, cy, radius, fill='#cccccc', stroke=null, stroke_
 
 
 export function drawFretboard(selector, instrument, notes) {
-    let tuning = instrument.tuning;
-    let string_gauges = instrument.string_gauges;
-    let fret_count = instrument.fret_count;
+    const tuning = instrument.tuning;
+    const string_gauges = instrument.string_gauges;
+    const fret_count = instrument.fret_count;
 
-    let svg = D3Select(selector);
+    const svg = D3Select(selector);
     svg.selectAll('*').remove();
 
-    let bbox = svg.node().parentElement.getBoundingClientRect();
+    const bbox = svg.node().parentElement.getBoundingClientRect();
     let width = bbox.width * 0.95;
 
     // Readjust width so the fretboard works for different screen sizes and fret counts
-    let min_fret_distance = 26;
-    let max_fret_distance = 140;
-    let min_width = min_fret_distance * (fret_count + 1);
-    let max_width = max_fret_distance * (fret_count + 1);
+    const min_fret_distance = 26;
+    const max_fret_distance = 140;
+    const min_width = min_fret_distance * (fret_count + 1);
+    const max_width = max_fret_distance * (fret_count + 1);
     width = Math.min(max_width, Math.max(min_width, width));
 
     // Calculate fret and string distances based on container width
-    let fret_distance = width / (fret_count + 1);
-    let string_distance = fret_distance * 0.65;
+    const fret_distance = width / (fret_count + 1);
+    const string_distance = fret_distance * 0.65;
 
     // Calculate paddings and margins based on fret and string distances
-    let string_padding = string_distance * 0.1;
-    let fret_padding = fret_distance * 0.01;
-    let margin_horizontal = fret_distance * 0.8;
-    let margin_vertical = string_distance * 0.5;
+    const string_padding = string_distance * 0.1;
+    const fret_padding = fret_distance * 0.01;
+    const margin_horizontal = fret_distance * 0.8;
+    const margin_vertical = string_distance * 0.5;
 
-    let height = string_distance * tuning.length + margin_vertical;
-    let string_width = width - margin_horizontal * 1.2;
-    let fret_height = string_distance * (tuning.length - 1);
-    let fret_width = fret_distance * 0.06;
+    const height = string_distance * tuning.length + margin_vertical;
+    const string_width = width - margin_horizontal * 1.2;
+    const fret_height = string_distance * (tuning.length - 1);
+    const fret_width = fret_distance * 0.06;
 
-    let note_radius = fret_distance * 0.22;
-    let note_stroke_width = note_radius * 0.2;
-    let note_font_size = note_radius * 1.05;
+    const note_radius = fret_distance * 0.23;
+    const note_stroke_width = note_radius * 0.1;
+    const note_font_size = note_radius * 1.05;
 
-    let fret_marker_radius = note_radius * 0.3;
+    const fret_marker_radius = note_radius * 0.3;
 
     // Setup SVG
     svg.attr('width', width).attr('height', height);
-    let transform = `translate(${margin_horizontal}, ${margin_vertical})`;
+    const transform = `translate(${margin_horizontal}, ${margin_vertical})`;
 
-    let g_frets = svg.append('g').attr('class', 'frets').attr('transform', transform);
-    let g_fret_markers = svg.append('g').attr('class', 'fret-markers').attr('transform', transform);
-    let g_strings = svg.append('g').attr('class', 'strings').attr('transform', transform);
-    let g_notes = svg.append('g').attr('class', 'notes').attr('transform', transform);
+    const g_frets = svg.append('g').attr('class', 'frets').attr('transform', transform);
+    const g_fret_markers = svg.append('g').attr('class', 'fret-markers').attr('transform', transform);
+    const g_strings = svg.append('g').attr('class', 'strings').attr('transform', transform);
+    const g_notes = svg.append('g').attr('class', 'notes').attr('transform', transform);
 
     // draw frets
     for (let i = 0; i <= fret_count; i++) {
@@ -91,7 +91,7 @@ export function drawFretboard(selector, instrument, notes) {
         let cy = height - margin_vertical - fret_marker_radius * 3;
 
         if (0 == i % 12) {
-            let offset = fret_marker_radius * 1.3;
+            const offset = fret_marker_radius * 1.3;
             drawCircle(g_fret_markers, cx + offset, cy, fret_marker_radius);
             // make sure the next circle is offset in the other direction
             cx -= offset;
@@ -102,8 +102,8 @@ export function drawFretboard(selector, instrument, notes) {
 
     // draw strings
     for (let i = 0; i < tuning.length; i++) {
-        let root = tuning[i];
-        let offset = i * string_distance;
+        const root = tuning[i];
+        const offset = i * string_distance;
         g_strings.append('svg:line')
             .attr('x1', -fret_padding / 2)
             .attr('y1', offset)
@@ -116,29 +116,29 @@ export function drawFretboard(selector, instrument, notes) {
 
     // draw notes
     for (let string_idx = 0; string_idx < tuning.length; string_idx++) {
-        let root_idx = noteIndex(tuning[string_idx]);
+        const root_idx = noteIndex(tuning[string_idx]);
         for (let fret_idx = 0; fret_idx <= fret_count; fret_idx++) {
-            let note = noteAtPosition(root_idx, fret_idx, notes);
+            const note = noteAtPosition(root_idx, fret_idx, notes);
             if (!note) continue;
 
-            let cx = fret_idx * fret_distance - fret_distance * 0.5;
-            let cy = string_idx * string_distance;
-            let x = cx - note_font_size * 0.34;
-            let y = cy + note_font_size * 0.34;
+            const label = noteLabel(note);
+            // Adjust font size based on label length to better fit sharps and flats.
+            const reduce = 5 * (1 - 1 / label.length);
+            const font_size = note_font_size - reduce;
 
-            // shift to left more for sharps and flats
-            if (note.length > 1) {
-                x = cx - note_font_size * 0.5;
-            }
+            const cx = fret_idx * fret_distance - fret_distance * 0.5;
+            const cy = string_idx * string_distance;
 
-            drawCircle(g_notes, cx, cy, note_radius, '#ffffff', '#aaaaaa', note_stroke_width, note);
+            drawCircle(g_notes, cx, cy, note_radius, '#ffffff', '#999999', note_stroke_width, note);
 
             g_notes.append('svg:text')
-                .attr('x', x)
-                .attr('y', y)
+                .attr('x', cx)
+                .attr('y', cy)
+                .attr('dy', '0.33em')
                 .attr('fill', '#000000')
-                .style('font-size', `${note_font_size}px`)
-                .text(noteLabel(note));
+                .style('text-anchor', `middle`)
+                .style('font-size', `${font_size}px`)
+                .text(label);
         }
     }
 }
